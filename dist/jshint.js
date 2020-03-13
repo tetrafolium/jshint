7261922fcd58bcb32aca8b7142236a59b461b792
Inspecode <inspecode-bot@rocro.com>
2020-03-13 09:58:22 +0000

    Fix ESLint errors

--- dist/jshint.js
+++ dist/jshint.js
@@ -1500,7 +1500,7 @@ function now() {
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  */
-;(function() {
+(function() {
 
   /** Used as a safe reference for `undefined` in pre-ES5 environments. */
   var undefined;
